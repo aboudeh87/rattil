bee8537e5db3383ccf66aa2348a93e692e9fe914
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-03 02:25:36 +0300

    fix nullable function

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ class CreateUsersTable extends Migration
         {
             $table->increments('id');
             $table->string('name');
-            $table->string('username', 254)->unique()->nulalble()->default(null);
-            $table->string('email', 254)->unique()->nulalble()->default(null);
+            $table->string('username', 254)->unique()->nullable()->default(null);
+            $table->string('email', 254)->unique()->nullable()->default(null);
             $table->string('password');
             $table->rememberToken();
             $table->string('api_token', 100)->nullable();
