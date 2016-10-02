9ced56c3db5e5ebf5b5c1f189267ed222e497c58
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-02 23:54:14 +0300

    SocialMedia model

---
+++ app/SocialMedia.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+/**
+ * Class SocialMedia
+ */
+class SocialMedia extends Authenticatable
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'social_id',
+        'provider',
+        'token',
+        'secret',
+        'nickname',
+        'email',
+        'avatar',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'token',
+        'secret',
+    ];
+}
