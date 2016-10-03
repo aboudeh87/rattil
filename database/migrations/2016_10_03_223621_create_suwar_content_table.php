d79399ec2f96634c8140d18aa8a871ecabf3fb24
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-04 01:46:56 +0300

    Add suwar_content table migration

---
+++ database/migrations/2016_10_03_223621_create_suwar_content_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuwarContentTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suwar_content', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('sura_id', false, true);
+            $table->string('language', 2);
+            $table->string('name', 255);
+            $table->text('definition')->nullable();
+            $table->timestamps();
+
+            $table->unique(['sura_id', 'language']);
+            $table->foreign('sura_id')->references('id')->on('suwar')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('language')->references('key')->on('languages')->onUpdate('CASCADE')->onDelete('RESTRICT');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suwar_content');
+    }
+}
