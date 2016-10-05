4a92b36da5f34cb25722368d52cc40928d42cf0c
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-06 01:14:22 +0300

    make Recitation as likable model

--- app/Recitation.php
+++ app/Recitation.php
@@ -3,31 +3,33 @@
 namespace App;
 
 
+use App\Traits\Likable;
+use App\Contracts\LikableContract;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * App\Recitation
  *
- * @property integer $id
- * @property integer $user_id
- * @property integer $sura_id
- * @property integer $narration_id
- * @property integer $from_verse
- * @property integer $to_verse
- * @property string $slug
- * @property string $description
- * @property string $url
- * @property string $length
- * @property boolean $verified
- * @property boolean $disabled
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
- * @property string $deleted_at
- * @property-read \App\User $user
- * @property-read \App\Sura $sura
+ * @property integer             $id
+ * @property integer             $user_id
+ * @property integer             $sura_id
+ * @property integer             $narration_id
+ * @property integer             $from_verse
+ * @property integer             $to_verse
+ * @property string              $slug
+ * @property string              $description
+ * @property string              $url
+ * @property string              $length
+ * @property boolean             $verified
+ * @property boolean             $disabled
+ * @property \Carbon\Carbon      $created_at
+ * @property \Carbon\Carbon      $updated_at
+ * @property string              $deleted_at
+ * @property-read \App\User      $user
+ * @property-read \App\Sura      $sura
  * @property-read \App\Narration $narration
- * @property-read \App\Verse $fromVerse
- * @property-read \App\Verse $toVerse
+ * @property-read \App\Verse     $fromVerse
+ * @property-read \App\Verse     $toVerse
  * @method static \Illuminate\Database\Query\Builder|\App\Recitation whereId($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Recitation whereUserId($value)
  * @method static \Illuminate\Database\Query\Builder|\App\Recitation whereSuraId($value)
@@ -45,8 +47,10 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Query\Builder|\App\Recitation whereDeletedAt($value)
  * @mixin \Eloquent
  */
-class Recitation extends Model
+class Recitation extends Model implements LikableContract
 {
+
+    use Likable;
 
     /**
      * The table associated with the model.
