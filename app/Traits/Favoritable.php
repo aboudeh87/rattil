9c5eba1282120f8cc4282a681a81f565b5670c0c
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-06 01:26:48 +0300

    Add favoritable trait

---
+++ app/Traits/Favoritable.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Traits;
+
+
+use App\Favorite;
+
+/**
+ * Class Favoritable
+ *
+ * @package App\Traits
+ */
+trait Favoritable
+{
+
+    /**
+     * get the users who add the model to favorite
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function favoritors()
+    {
+        return $this->morphMany(Favorite::class, 'likable');
+    }
+}
