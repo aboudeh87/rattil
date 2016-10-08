d7d3671724d98c3b6d1d22e41cb15c3a93eac2c3
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-09 00:31:45 +0300

    Follow model

---
+++ app/Follow.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Follow
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $followable_type
+ * @property integer $followable_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \App\User $user
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $model
+ * @method static \Illuminate\Database\Query\Builder|\App\Follow whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Follow whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Follow whereFollowableType($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Follow whereFollowableId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Follow whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Follow whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Follow extends Model
+{
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'followers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['followable_type', 'followable_id', 'user_id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function model()
+    {
+        return $this->morphTo('followable');
+    }
+}
