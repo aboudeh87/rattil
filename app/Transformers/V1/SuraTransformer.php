64351c387a4b364650c5f8e363724dffe7fee0da
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-10 00:00:34 +0300

    Sura transformer

---
+++ app/Transformers/V1/SuraTransformer.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Transformers\V1;
+
+
+use App\Sura;
+use App\SuraContent;
+use App\Transformers\Transformer;
+use App\Verse;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class SuraTransformer
+ *
+ * @package App\Transformers\V1
+ */
+class SuraTransformer extends Transformer
+{
+
+    /**
+     * Transform single item
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     *
+     * @return array
+     */
+    protected function transformItem(Model $model)
+    {
+        /**
+         * @var Sura        $model
+         * @var SuraContent $content
+         */
+        $content = SuraContent::whereSuraId($model->id)
+            ->whereLanguageKey(\App::getLocale())
+            ->first() ?: SuraContent::whereSuraId($model->id)->first();
+
+        $verses = $model->verses()->orderBy('number', 'asc')->map(function (Verse $verse)
+        {
+            return [
+                'id'         => (int) $verse->id,
+                'number'     => (int) $verse->number,
+                'text'       => $verse->text,
+                'cleanText'  => $verse->clean_text,
+                'characters' => $verse->characters,
+            ];
+        });
+
+        return [
+            'id'                 => (int) $model->id,
+            'name'               => $content->name,
+            'revealed'           => trans("labels.{$model->revealed}"),
+            'chronologicalOrder' => (int) $model->chronological_order,
+            'verses'             => $verses,
+        ];
+    }
+}
