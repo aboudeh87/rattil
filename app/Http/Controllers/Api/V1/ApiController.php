c698bd3eae4a06c0a7e626e35c4fb8d8a358ac39
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-10 00:30:49 +0300

    add respond with pagination method

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -3,49 +3,17 @@
 namespace App\Http\Controllers\Api\V1;
 
 
-use Closure;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use App\Transformers\Transformer;
 use App\Http\Controllers\Controller;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Contracts\Pagination\Paginator;
 
 /**
  * Class ApiController
  *
- * @package GeniusTS\Core
+ * @package App\Http\Controllers\Api\V1
  */
 class ApiController extends Controller
 {
-
-    /**
-     * @var Request
-     */
-    protected $request;
-
-    /**
-     * @var integer
-     */
-    protected $limit;
-
-    /**
-     * @var string
-     */
-    protected $keyword;
-
-    /**
-     * ApiController constructor.
-     *
-     * @param Request $request
-     */
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-        $keyword = trim($this->request->get('keyword', null));
-
-        $this->keyword = $keyword ? '%' . str_replace(' ', '%', $keyword) . '%' : null;
-        $this->limit = (int) trim($this->request->get('limit', 15));
-    }
 
     /**
      * return a JSON response
@@ -56,9 +24,29 @@ class ApiController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function respond(array $data, $code = 200, array $headers = [])
+    protected function respond(array $data, $code = 200, array $headers = [])
     {
         return response()->json($data, $code, $headers);
+    }
+
+    /**
+     * return a response with pagination
+     *
+     * @param \Illuminate\Contracts\Pagination\Paginator $paginate
+     * @param \App\Transformers\Transformer              $transformer
+     * @param int                                        $code
+     * @param array                                      $headers
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithPagination(Paginator $paginate, Transformer $transformer, $code = 200, $headers = [])
+    {
+        return $this->respond([
+            'total'       => $paginate->total(),
+            'limit'       => $paginate->perPage(),
+            'currentPage' => $paginate->currentPage(),
+            'data'        => $transformer->transform($paginate->getCollection())->toArray(),
+        ], $code, $headers);
     }
 
     /**
@@ -70,7 +58,7 @@ class ApiController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function respondError($errors, $code = 400, array $headers = [])
+    protected function respondError($errors, $code = 400, array $headers = [])
     {
         return $this->respond(['success' => false, 'message' => $errors], $code, $headers);
     }
@@ -84,7 +72,7 @@ class ApiController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function respondSuccess($messages, $code = 200, array $headers = [])
+    protected function respondSuccess($messages, $code = 200, array $headers = [])
     {
         return $this->respond(['success' => true, 'message' => $messages], $code, $headers);
     }
