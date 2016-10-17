c5fd820b7fdc0b94d918cc78d6dcf608ed212a8f
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-17 21:34:19 +0300

    Fix PHPDoc

--- app/User.php
+++ app/User.php
@@ -11,42 +11,24 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 /**
  * App\User
  *
- * @property integer
- *               $id
- * @property string
- *               $name
- * @property string
- *               $username
- * @property string
- *               $email
- * @property string
- *               $avatar
- * @property string
- *               $password
- * @property string
- *               $remember_token
- * @property string
- *               $api_token
- * @property \Carbon\Carbon
- *               $created_at
- * @property \Carbon\Carbon
- *               $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\SocialMedia[]
- *                    $socials
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Comment[]
- *                    $comments
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Like[]
- *                    $likes
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Favorite[]
- *                    $favorites
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Follow[]
- *                    $following
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Follow[]
- *                    $followers
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[]
- *                $notifications
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[]
- *                $unreadNotifications
+ * @property integer $id
+ * @property string $name
+ * @property string $username
+ * @property string $email
+ * @property string $avatar
+ * @property string $password
+ * @property string $remember_token
+ * @property string $api_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\SocialMedia[] $socials
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Comment[] $comments
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Like[] $likes
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Favorite[] $favorites
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Follow[] $following
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Follow[] $followers
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $unreadNotifications
  * @method static \Illuminate\Database\Query\Builder|\App\User whereId($value)
  * @method static \Illuminate\Database\Query\Builder|\App\User whereName($value)
  * @method static \Illuminate\Database\Query\Builder|\App\User whereUsername($value)
