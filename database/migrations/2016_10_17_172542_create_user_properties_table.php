3722ff48e44ee0b532e7fec1ec328352aefbdf5b
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-17 20:35:47 +0300

    create user_properties table migration

---
+++ database/migrations/2016_10_17_172542_create_user_properties_table.php
@@ -0,0 +1,38 @@
+
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_properties', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id', false, true);
+            $table->string('key');
+            $table->text('value')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_properties');
+    }
+}
