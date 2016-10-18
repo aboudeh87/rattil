0bd289f47de99b87529a61bf27831d9643a0d7a8
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-18 21:11:59 +0300

    Add FollowerTransformer

---
+++ app/Transformers/V1/FollowerTransformer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Transformers\V1;
+
+
+use App\User;
+use App\Transformers\Transformer;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class FollowerTransformer
+ *
+ * @package App\Transformers\V1
+ */
+class FollowerTransformer extends Transformer
+{
+
+    /**
+     * Transform single item
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     *
+     * @return array
+     */
+    protected function transformItem(Model $model)
+    {
+        $country = $model->properties()->where('key', 'country')->first();
+
+        /** @var User $model */
+        return [
+            'id'                => (int) $model->id,
+            'name'              => $model->name,
+            'username'          => $model->username,
+            'avatar'            => $model->avatar,
+            'country'           => $country ? $country->value : null,
+            'recitations_count' => isset($model->recitation_count) ?
+                $model->recitation_count : $model->recitations()->count(),
+        ];
+    }
+}
