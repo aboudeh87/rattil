3e216b2a5c65ec9a368b56fa078c551cb7a60d84
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-18 21:17:17 +0300

    Add following users list method

---
+++ app/Http/Controllers/Api/V1/FollowersController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+
+use App\Traits\JsonResponses;
+use App\Traits\ProfilesChecker;
+use App\Transformers\V1\FollowerTransformer;
+
+class FollowersController extends ApiController
+{
+
+    use ProfilesChecker, JsonResponses;
+
+    /**
+     * Return The following users of a specific user
+     *
+     * @param null|string $model
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function following($model = null)
+    {
+        if (!$this->isAllowed($model))
+        {
+            return $this->accessDeniedResponse();
+        }
+
+        return $this->respondWithPagination(
+            $this->model->following()->withCount('recitations')->paginate(),
+            new FollowerTransformer
+        );
+    }
+}
