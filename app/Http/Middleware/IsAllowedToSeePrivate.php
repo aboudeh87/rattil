0eb28094f2280f8aec20eadb3be428cbc3737fd4
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-21 00:09:09 +0300

    Add IsAllowedToSeePrivate middleware

---
+++ app/Http/Middleware/IsAllowedToSeePrivate.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+
+class IsAllowedToSeePrivate
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     * @param null|string               $guard
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        $model = $request->route()->parameter('model');
+        $private = $model->user->properties()->where('key', 'private')->first();
+        $isFollower = $model->user
+            ->followers()
+            ->where('user_id', auth($guard)->id())
+            ->where('accepted', true)
+            ->count();
+
+        if ($private && $private->value && !$isFollower)
+        {
+            return new JsonResponse([
+                'success' => false,
+                'message' => trans('messages.privacy_access_denied'),
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
