57c1f50a18d56f659098897da69bd65b9d8516f8
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-10-20 23:43:41 +0300

    Update accessDenied response message

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,7 +2,7 @@
 
 return [
     'posted_success'           => 'Your recitation was added successfully.',
-    'privacy_access_denied'    => 'you cannot access this profile, it is a private profile.',
+    'privacy_access_denied'    => 'you cannot access this :module, it is a private :module.',
     'profile_updated_success'  => 'Your profile updated successfully.',
     'avatar_uploaded_success'  => 'Your new profile image uploaded successfully.',
     'avatar_deleted_success'   => 'Your profile image deleted successfully.',
